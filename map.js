04c8759b64ca18d26163be22d7d07efc8ee18dc8
Omar Cesar <omar-nav@users.noreply.github.com>
2018-11-02 13:03:27 -0600

    change initial zoom level

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([0,-16], 5);
+var mymap = L.map('mapid').setView([0,-16], 2.5);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
